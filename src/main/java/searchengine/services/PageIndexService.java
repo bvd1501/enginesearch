67ed131686366670fd77e0b0cd44bff38b7dd507
boris <bvd1501@gmail.com>
2023-09-04 16:22:41 +0400

    оптимизация PageIndexService: изменен метод compute, выделен метод savePage, переписан метод pageHandler (ранее pageExtractor), изменение логики получения пути страницы из полного адреса

--- src/main/java/searchengine/services/PageIndexService.java
+++ src/main/java/searchengine/services/PageIndexService.java
@@ -34,49 +34,50 @@ public class PageIndexService extends RecursiveAction {
     private final JsoupCfg jsoupCfg;
 
     private final SiteEntity site;
-    private final URL pageURL;
-
-    //private final ForkJoinPool siteFJPool;
+    private final String pageAddress;
 
 
     @Autowired
-    public PageIndexService(ApplicationContext context, SiteEntity site, URL pageURL) {
+    public PageIndexService(ApplicationContext context, SiteEntity site, String pageAddress) {
         this.context = context;
         this.pageRepo = context.getBean(PageRepo.class);
         this.jsoupCfg = context.getBean(JsoupCfg.class);
         this.siteIndexingService = context.getBean(SiteIndexingService.class);
         this.site = site;
-        this.pageURL = pageURL;
-        //this.siteFJPool = siteFJPool;
+        this.pageAddress = pageAddress;
     }
 
 
     @Override
     protected void compute() {
         if (siteIndexingService.isStopFlag()) {return;}
-        if (pageRepo.findBySiteAndPath(site, pageURL.getPath()).isPresent()) return;
         try {
-            Connection.Response response = connector(pageURL);
-            synchronized (pageRepo) {
-                if (pageRepo.findBySiteAndPath(site, pageURL.getPath()).isPresent()) return;
-                pageRepo.save(new PageEntity(site, pageURL.getPath(), response.statusCode(), response.body()));
-                siteIndexingService.saveSite(site, StatusType.INDEXING, "");
-            }
-            if (response.statusCode() != 200) return;
+            Connection.Response response = connector();
+            if (!savePage(response) || response.statusCode()!=200) return;
             //TODO запуск лемантизатора для индексации содержимого страницы (в отдельном потоке???)
-            ForkJoinTask.invokeAll(pageExtractor(response.parse()));
+            ForkJoinTask.invokeAll(pageHandler(response.parse()));
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException("Internal error on timeout");
         }
     }
 
 
+    private boolean savePage(Connection.Response response) {
+        String pathPage = "/" + URI.create(site.getUrl()).relativize(URI.create(pageAddress));
+        //log.info("page:" + pageAddress + "....path:" + pathPage);
+        synchronized (pageRepo) {
+            if (pageRepo.findBySiteAndPath(site, pathPage).isPresent()) return false;
+            pageRepo.save(new PageEntity(site, pathPage, response.statusCode(), response.body()));
+            siteIndexingService.saveSite(site, StatusType.INDEXING, "");
+        }
+        return true;
+    }
 
-    private Connection.Response connector(URL pageAddress) throws IOException, InterruptedException {
+    private Connection.Response connector() throws IOException, InterruptedException {
         int timeout = jsoupCfg.getTimeoutMin() + (int) (Math.random()*(jsoupCfg.getTimeoutMax()-jsoupCfg.getTimeoutMin()));
         Thread.sleep(timeout);
         return Jsoup
-                .connect(pageAddress.toString())
+                .connect(pageAddress)
                 .userAgent(jsoupCfg.getUserAgent())
                 .referrer(jsoupCfg.getReferrer())
                 .timeout(jsoupCfg.getTimeout())
@@ -88,33 +89,31 @@ public class PageIndexService extends RecursiveAction {
 
 
 
-    private HashSet<PageIndexService> pageExtractor(Document inputDoc) {
+    private HashSet<PageIndexService> pageHandler(Document inputDoc) {
         HashSet<PageIndexService> resultPageServices = new HashSet<>();
         Elements elements = inputDoc.select("a");
         for (Element element : elements) {
-            String linkS = element.absUrl("href").toLowerCase().replaceAll(" ", "%20");
-            if (!linkS.startsWith(site.getUrl())) {continue;}
+            String link = element.absUrl("href").toLowerCase().replaceAll(" ", "%20");
+            if (!link.startsWith(site.getUrl()) || !isLinkValid(link)) {continue;}
             try {
-                URL fullURL = URI.create(linkS).toURL();
-                URL linkURL = URI.create(fullURL.getProtocol() + "://" + fullURL.getHost() + fullURL.getPath()).toURL();
-                if (isLinkValid(linkURL) && pageRepo.findBySiteAndPath(site, linkURL.getPath()).isEmpty())  {
-                    resultPageServices.add(context.getBean(PageIndexService.class, context, site, linkURL));
+                URI fullLinkURI = URI.create(link);
+                String cleanLink = fullLinkURI.getScheme() + "://" + fullLinkURI.getRawAuthority() + fullLinkURI.getPath();
+                String pathLink = "/" + URI.create(site.getUrl()).relativize(URI.create(cleanLink));
+                //log.info("handlerLog: link=" + cleanLink);
+                //log.info("==========: path=" + pathLink);
+                if (pageRepo.findBySiteAndPath(site, pathLink).isEmpty())  {
+                    resultPageServices.add(context.getBean(PageIndexService.class, context, site, cleanLink));
                     }
-            } catch (MalformedURLException | IllegalArgumentException e) {
-                log.error(linkS + " :: " + e.getMessage());
+            } catch (IllegalArgumentException e) {
+                log.error(link + "->" + e.getMessage());
             }
         }
         return resultPageServices;
     }
 
-    private boolean isLinkValid(URL link) {
-        URL finalLink = link;
-        //if (!link.getProtocol().equals(pageURL.getProtocol())) {return false;}
-        String parentHost = pageURL.getHost().startsWith("www.") ? pageURL.getHost().substring(4) : pageURL.getHost();
-        String childHost = finalLink.getHost().startsWith("www.") ? finalLink.getHost().substring(4) : finalLink.getHost();
-        if (!childHost.equals(parentHost)) {return false;}
+    private boolean isLinkValid(String link) {
         return EnumSet.allOf(BadLinks.class).stream().noneMatch(enumElement ->
-                finalLink.getPath().contains(enumElement.toString()));
+                link.contains(enumElement.toString()));
     }
 
 
--- src/main/java/searchengine/services/SiteIndexingServiceImp.java
+++ src/main/java/searchengine/services/SiteIndexingServiceImp.java
@@ -1,7 +1,6 @@
 package searchengine.services;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
@@ -12,9 +11,6 @@ import searchengine.model.SiteEntity;
 import searchengine.model.StatusType;
 import searchengine.repo.PageRepo;
 import searchengine.repo.SiteRepo;
-
-import java.net.URI;
-import java.net.URL;
 import java.util.concurrent.*;
 
 
@@ -25,17 +21,14 @@ public class SiteIndexingServiceImp implements SiteIndexingService {
     private final SitesList sites;
     private final SiteRepo siteRepo;
     private final PageRepo pageRepo;
-    //public final PageIndexServiceFactory pageIndexServiceFactory;
-    //private final ObjectFactory<PageIndexService> pageIndexServiceObjectFactory;
-    private static boolean stopFlag;// volatile, = false;
+
+    private static boolean stopFlag;
 
     private ExecutorService siteExecutor;
 
     @Autowired
     public SiteIndexingServiceImp(ApplicationContext context) {
-        this.context = context;
-        //this.pageIndexServiceFactory = this.context.getBean(PageIndexServiceFactory.class);
-        //this.pageIndexServiceObjectFactory = pageIndexServiceObjectFactory;
+        this.context = context;     
         this.sites = this.context.getBean(SitesList.class);
         this.siteRepo = this.context.getBean(SiteRepo.class);
         this.pageRepo = this.context.getBean(PageRepo.class);
@@ -74,44 +67,29 @@ public class SiteIndexingServiceImp implements SiteIndexingService {
         // !(siteExecutor == null || (siteExecutor.isShutdown() && siteExecutor.isTerminated()))
         return siteExecutor != null && !siteExecutor.isTerminated();
     }
-
     @Override
     public boolean isStopFlag() {
         return stopFlag;
     }
-
     @Override
     public void resetStopFlag() {
         stopFlag = false;
     }
-
     @Override
     public void setStopFlag() {
         stopFlag = true;
     }
 
-    @Override
-    public void saveSite(SiteEntity siteEntity, StatusType statusType, String last_error) {
-        if (isStopFlag()) {
-            statusType = StatusType.FAILED;
-            last_error = "Stop by user";
-            log.error(siteEntity.getName() + " - stop by user");
-        }
-        siteEntity.setStatus(statusType);
-        siteEntity.setLast_error(last_error);
-        siteEntity.setStatusTime(new java.util.Date());
-        siteRepo.save(siteEntity);
-    }
 
 
-    private void siteIndexing(String urlSiteString, String nameSite) {
+    private void siteIndexing(String urlSite, String nameSite) {
         long startTime = System.currentTimeMillis();
         Thread.currentThread().setName("th." + nameSite);
-        SiteEntity currentSite = preStartSiteIndexing(urlSiteString, nameSite);
-        ForkJoinPool sitePool = new ForkJoinPool(Runtime.getRuntime().availableProcessors()-1);
-        //ForkJoinPool sitePool = ForkJoinPool.commonPool();
+        SiteEntity currentSite = preStartSiteIndexing(urlSite, nameSite);
+        //ForkJoinPool sitePool = new ForkJoinPool(Runtime.getRuntime().availableProcessors()-1);
+        ForkJoinPool sitePool = ForkJoinPool.commonPool();
         try {
-            URL urlSite = URI.create(urlSiteString).toURL();
+            //URI uriSite = URI.create(urlSite);
             var pageIndexService = context.getBean(PageIndexService.class, context, currentSite, urlSite);
             sitePool.invoke(pageIndexService);
             //sitePool.shutdown();
@@ -126,15 +104,26 @@ public class SiteIndexingServiceImp implements SiteIndexingService {
     }
 
 
-    private SiteEntity preStartSiteIndexing(String urlSiteString, String nameSite) {
+    private SiteEntity preStartSiteIndexing(String urlSite, String nameSite) {
         log.info("Start indexing site: " + nameSite);
-        siteRepo.deleteByUrlAndName(urlSiteString, nameSite);
-        SiteEntity currentSite = new SiteEntity(urlSiteString, nameSite);
+        siteRepo.deleteByUrlAndName(urlSite, nameSite);
+        SiteEntity currentSite = new SiteEntity(urlSite, nameSite);
         saveSite(currentSite, StatusType.INDEXING, null);
         return currentSite;
     }
 
-
+    @Override
+    public void saveSite(SiteEntity siteEntity, StatusType statusType, String last_error) {
+        if (isStopFlag()) {
+            statusType = StatusType.FAILED;
+            last_error = "Stop by user";
+            log.error(siteEntity.getName() + " - stop by user");
+        }
+        siteEntity.setStatus(statusType);
+        siteEntity.setLast_error(last_error);
+        siteEntity.setStatusTime(new java.util.Date());
+        siteRepo.save(siteEntity);
+    }
 
 
 }
